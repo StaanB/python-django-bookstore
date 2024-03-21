cc85df38ba3b25ea8317e9ce1eed17c4ad2f4655
Stanley <stanleybrenner@gmail.com>
2024-03-21 12:25:37 -0300

    alterações para pythonanywhere

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = os.getenv("DJANGO_ALLOWED_HOSTS", "").split(" ")
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "StaanB.pythonanywhere.com"]
 
 
 # Application definition
